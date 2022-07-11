2a2487d7654751f5f77ab48ced64c6cd23693684
hamomgs <hamomgomer947@gmail.com>
2022-07-11 20:30:15 -0300

    exercise complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import fruitImg from './assets/image/pera.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    firstName: 'Hamom',
+    age: 19,
+    favFood: 'Pão de Queijo',
+    musics: ['Let Her Go', 'Numb Little Bug', 'Alone']
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Nome: {this.state.firstName}</h1>
+        <h2>Idade: {this.state.age}</h2>
+        <h3>Comida favorita: {this.state.favFood}</h3>
+        <h3>3 Músicas:</h3>
+        <ul>
+          <li>{this.state.musics[0]}</li>
+          <li>{this.state.musics[1]}</li>
+          <li>{this.state.musics[2]}</li>
+        </ul>
+        <h3>Fruta favorita:</h3>
+        <img src={fruitImg} alt="Pera" />
+      </div>
+    );
+  }
 }
-
-export default App;
